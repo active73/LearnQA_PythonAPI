f0f5594a28a5716316deb33c8b09de00c1fdf5e5
Usaleva <usaleva@uchi.ru>
2021-06-26 14:28:32 +0400

    Ex6

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ import json
 json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
 obj = json.loads(json_text)
 print(obj)
+
+
+# Ex6
+response = requests.get("https://playground.learnqa.ru/api/long_redirect")
+result = response.history
+print(list(map(lambda x: x.url, result)))
