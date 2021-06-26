d52380c028fc2b476fd5e55130aaab93ae2d7896
Usaleva <usaleva@uchi.ru>
2021-06-26 13:44:47 +0400

    Ex5

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-print("Hello from Elena")
-
 import requests
+import json
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+# Ex5
+json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+obj = json.loads(json_text)
+print(obj)
